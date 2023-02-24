7295be803617647c3c6af0b32a3c99fd1405f899
Jörn Erbguth <joern@erbguth.net>
2023-02-24 11:29:53 +0100

    Create rest.py

---
+++ rest.py
@@ -0,0 +1,43 @@
+from flask import (
+    Flask,
+    render_template
+)
+import connexion
+
+# Create the application instance
+app = connexion.App(__name__, specification_dir='./')
+
+# Read the swagger.yml file to configure the endpoints
+app.add_api('openapi.yml')
+
+# Create a URL route in our application for "/"
+@app.route('/ui')
+def home():
+    """
+    This function just responds to the browser ULR
+    anyhost:5000/
+    :return:        the rendered template 'home.html'
+    """
+    return render_template('home.html')
+    
+app = connexion.App(__name__, specification_dir='./')
+
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
+
+# Create a URL route in our application for "/"
+@app.route('/ui')
+def home():
+    """
+    This function just responds to the browser ULR
+    anyhost:5000/
+    :return:        the rendered template 'home.html'
+    """
+    return render_template('home.html')
+
+
+from OpenSSL import SSL
+
+# If we're running in stand alone mode, run the application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
