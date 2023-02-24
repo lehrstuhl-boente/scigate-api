facb46c0274f46c5226283a8329834b8b9e12e1b
Jörn Erbguth <joern@erbguth.net>
2023-02-24 11:31:56 +0100

    Update rest.py

--- rest.py
+++ rest.py
@@ -7,8 +7,8 @@ import connexion
 # Create the application instance
 app = connexion.App(__name__, specification_dir='./')
 
-# Read the swagger.yml file to configure the endpoints
-app.add_api('openapi.yml')
+# Read the yaml file to configure the endpoints
+app.add_api('openapi.yaml')
 
 # Create a URL route in our application for "/"
 @app.route('/ui')
