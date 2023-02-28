936976ef8813907eacbef8c3f5a79d7f86ac61b4
Jörn Erbguth <joern@erbguth.net>
2023-02-28 03:06:52 +0100

    Update rest.py

--- rest.py
+++ rest.py
@@ -1,46 +1,78 @@
-from flask import (
-    Flask,
-    render_template
-)
-import connexion
-from connexion.resolver import RestyResolver
-from flask_cors import CORS
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
+import threading
+import time
+import json
+import api
 
-# Create the application instance
-app = connexion.App(__name__, specification_dir='./')
+hostName = ""
+serverPort = 5001
 
-# Read the yaml file to configure the endpoints
-app.add_api('openapi.yaml')
-CORS(app.app)
+class MyServer(BaseHTTPRequestHandler):
+	def do_OPTIONS(self):
+		self.send_response(204,"ok")
+		self.send_header('Access-Control-Allow-Credentials', 'true')
+		self.send_header('Access-Control-Allow-Origin', '*')
+		self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+		self.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-type, Origin, Accept")
+		self.send_header("Access-Control-Max-Age", "86400")
+		self.end_headers()
+		
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
+		with open("index.html") as f:
+			lines = f.readlines()
+		f.close()
+		for i in lines:
+			self.wfile.write(bytes(i, "utf-8"))
 
-# Create a URL route in our application for "/"
-@app.route('/ui')
-def home():
-    """
-    This function just responds to the browser ULR
-    anyhost:5001/
-    :return:        the rendered template 'home.html'
-    """
-    return render_template('index.html')
+	def do_POST(self):
+		self.send_response(200)
+		self.send_header("Content-type", "application/json; charset=utf-8")
+		self.send_header("Access-Control-Allow-Origin", "*")
+		self.send_header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		self.send_header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+		self.end_headers()
+		reply={}
+		if "application/json" in self.headers.get("Content-type").lower():
+			data = self.rfile.read(int(self.headers.get('Content-Length')))
+			sdata=json.loads(data)
+			if 'engine' in sdata:
+				engine=sdata['engine']
+				if engine=='entscheidsuche':
+					reply=entscheidsuche.execute(sdata)
+				elif engine=='boris':
+					reply=boris.execute(sdata)
+				elif engine=='zora':
+					reply=zora.execute(sdata)
+				elif engine=='swisscovery':
+					reply=swisscovery.execute(sdata)
+				else:
+					reply['error']='engine '+engine+' unknown'
+		else:
+			reply['error']='unexpected Content-type: '+self.headers.get("Content-type")
+		if 'error' in reply:
+			reply['status']='error'
+		else:
+			reply['status']='ok'
+		string=json.dumps(reply, ensure_ascii=False).encode('utf8')
+		print(string);
+		self.wfile.write(string)
+		
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+    #server.serve_forwever()
     
-app = connexion.App(__name__, specification_dir='./')
+if __name__ == "__main__":        
+    webServer = ThreadedHTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
 
-# Read the swagger.yml file to configure the endpoints
-app.add_api('openapi.yaml')
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
 
-# Create a URL route in our application for "/"
-@app.route('/ui')
-def home():
-    """
-    This function just responds to the browser ULR
-    anyhost:5001/
-    :return:        the rendered template 'home.html'
-    """
-    return render_template('index.html')
-
-
-from OpenSSL import SSL
-
-# If we're running in stand alone mode, run the application
-if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    webServer.server_close()
+    print("Server stopped.")
