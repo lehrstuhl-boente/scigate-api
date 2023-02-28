7402868205fe270127943e51254ee9b32cea7c70
Jörn Erbguth <joern@erbguth.net>
2023-02-28 04:42:26 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,31 @@
 # function called by the API
 import json
+import requests
+
+APIURL="http://v2202109132150164038.luckysrv.de:8080/"
 
 def search(sdata):
 	reply={}
 	reply['status']='ok'
+	try:
+		collection=sdata['collection']
+		if collection in ['entscheidsuche','boris','zora','swisscovery']:
+			query=sdata['query']
+			maxHits=100
+			if 'maxHits' in sdata:
+				maxHits=sdata['maxHits']
+			maxReply=100
+			if 'maxReply' in sdata:
+				maxReply=sdata['maxReply']
+			result={}
+			data={'engine': collection, 'type': 'search', 'term': query}
+			r=requests.post(url=APIURL,data=data)
+			reply['ergebnis']=r.text 				
+		else:
+			reply['status']='error'
+			reply['error']="Collection '"+collection+"' unknown"	
+	catch:
+		reply['status']='error'
 	return reply
 	
 def load(sdata):
