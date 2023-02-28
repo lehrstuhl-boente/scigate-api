a2a3304d483b3a2871452484c046a09308bf61ac
Jörn Erbguth <joern@erbguth.net>
2023-02-28 03:42:36 +0100

    Update rest.py

--- rest.py
+++ rest.py
@@ -59,7 +59,7 @@ class MyServer(BaseHTTPRequestHandler):
 		print(string);
 		self.wfile.write(string)
 		
-	self.do_Common(self,sdata,command)
+	self.do_Common(self,sdata,command):
 		self.do_Header()
 		reply={}
 		reply['status']='ok'
