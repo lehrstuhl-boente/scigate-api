5771527062ff71a4359477b084fb6ff6c0ddd458
Jörn Erbguth <joern@erbguth.net>
2023-02-28 01:47:29 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,13 +1,13 @@
 # function called by the API
 import json
 
-def search(jsonBody):
+def search(**kwargs):
 	return jsonBody
 	
-def load(jsonBody):
+def load(**kwargs):
 	return jsonBody
 	
-def queryStatus(jsonBody):
+def queryStatus(**kwargs):
 	return jsonBody
 
 
