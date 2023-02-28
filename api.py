aced9a537175122407f867b06c5cb4228d2b2a03
Jörn Erbguth <joern@erbguth.net>
2023-02-28 02:23:28 +0100

    Update api.py

--- api.py
+++ api.py
@@ -4,16 +4,16 @@ import json
 def search(**kwargs):
 	jsonInput=json.loads(list(kwargs.values())[0].decode('utf-8'))
 	print(jsonInput)
-	return json.dumps({})
+	return json.dumps(jsonInput)
 	
 def load(**kwargs):
 	jsonInput=json.loads(list(kwargs.values())[0].decode('utf-8'))
 	print(jsonInput)
-	return json.dumps({})
+	return json.dumps(jsonInput)
 	
 def queryStatus(**kwargs):
 	jsonInput=json.loads(list(kwargs.values())[0].decode('utf-8'))
 	print(jsonInput)
-	return json.dumps({})
+	return json.dumps(jsonInput)
 
 
