698296ca39d29c01c3b30ddc35fec4a3ef270c65
Jörn Erbguth <joern@erbguth.net>
2023-03-15 05:18:14 +0100

    ui

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 function init(){
-    set_urls();
-}
-
-function set_urls(){
-	urlstart=location.protocol+'//'+location.hostname+'/api/';
-	document.getElementById('logo').src=urlstart+'logo.svg';
-	document.getElementById('css').href=urlstart+'index.css';
+	alert("init");
 }
