444c6cc664f6b6b961439187cb3d2e7d9af15348
Jörn Erbguth <joern@erbguth.net>
2023-04-30 02:14:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function update_parameter(){
 	parameter['collection']=document.querySelector('input[name="collection"]:checked').value;
 	parameter['query']=document.getElementById('searchterm').value;
 	parameter['filter']=document.getElementById('filter').value;
-	parameter['date']=document.getElementById('date').value;	
 	parameter['maxHits']=document.getElementById('maxHits').value;
 	parameter['maxReply']=document.getElementById('maxReply').value;
 	parameter['getDocs']=document.getElementById('getDocs').checked;
